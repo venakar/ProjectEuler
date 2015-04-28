d2c5379f4fb39619910049f796660272aa0f423b
venakar <venkat.techno@gmail.com>
2015-04-28 12:52:23 -0400

    Prob 24

---
+++ Prob_24.java
@@ -0,0 +1,70 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Lexicographic permutations
+ * Problem 24
+ * A permutation is an ordered arrangement of objects. 
+ * For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. 
+ * If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. 
+ * The lexicographic permutations of 0, 1 and 2 are: 012   021   102   120   201   210
+ * What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+ */
+
+/**
+ * @author p541832
+ *
+ */
+public class Prob_24 {
+
+	/**
+	 * 
+	 */
+	public Prob_24() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args 2783915460
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<String> hs=new HashSet<>(permutationFinder("0123456789"));
+		List sortedList = new ArrayList(hs);
+		Collections.sort(sortedList);
+		System.out.println(sortedList.get(999999));//2783915460
+	}
+	
+	 public static Set<String> permutationFinder(String str) {
+	        Set<String> perm = new HashSet<String>();
+	        //Handling error scenarios
+	        if (str == null) {
+	            return null;
+	        } else if (str.length() == 0) {
+	            perm.add("");
+	            return perm;
+	        }
+	        char initial = str.charAt(0); // first character
+	        String rem = str.substring(1); // Full string without first character
+	        Set<String> words = permutationFinder(rem);
+	        for (String strNew : words) {
+	            for (int i = 0;i<=strNew.length();i++){
+	                perm.add(charInsert(strNew, initial, i));
+	            }
+	        }
+	        return perm;
+	    }
+
+	    public static String charInsert(String str, char c, int j) {
+	        String begin = str.substring(0, j);
+	        String end = str.substring(j);
+	        return begin + c + end;
+	    }
+
+}
